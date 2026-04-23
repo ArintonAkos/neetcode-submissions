8f35f0d97dafba5c708b1d1a33740aaf0b378c9d
Arinton Akos <43067524+ArintonAkos@users.noreply.github.com>
2026-04-23 23:46:33 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        deps = defaultdict(list)
+        indegree = defaultdict(int)
+
+        # b source a sink
+        # b -> a
+
+        for a, b in prerequisites:
+            deps[b].append(a)
+            indegree[a] += 1
+
+        q = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                q.append(course)
+
+        res = []
+
+        while q:
+            item = q.popleft()
+            res.append(item)
+
+            for dep in deps[item]:
+                indegree[dep] -= 1
+                if indegree[dep] == 0:
+                    q.append(dep)
+        
+        # print(f"Res: {res} | Prerequisites: {prerequisites} | deps: {deps} | Indegree: {indegree}")
+
+        return len(res) == numCourses
+
+
